a3c08f73942e3d255b1f5a8f44d6d821a6483209
Yeray Lopez <ylopezjordano@gmail.com>
2023-03-20 14:23:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  let [lista, setLista] = useState([])
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?offset=20&limit=20')
+      .then(async response => {
+        const data = await response.json();
+        let array = []
+        for (let pokemon in data.results) {
+          array.push(<li key={data.results[pokemon].name}>{data.results[pokemon].name}</li>)
+        }
+        setLista(array)
+      })
+  }, [] )
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {lista}
+        </ul>
       </header>
     </div>
   );
