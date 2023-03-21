a20e919e8f1d519b74c094966d76e0d00285c40a
Yeray Lopez <ylopezjordano@gmail.com>
2023-03-21 21:11:18 +0100

    Added some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('changes page when nav button is clicked', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            results: [{ name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' }],
+            next: 'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20',
+            previous: null,
+          }),
+      })
+    );
+    render(<App />);
+    const nextButton = await screen.findByRole('button', { name: /next/i });
+    fireEvent.click(nextButton);
+    expect(global.fetch).toHaveBeenCalledTimes(2);
+    expect(global.fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon?offset=20&limit=20');
+  });
+  
+  test('fetches pokemon list on load', () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            results: [
+              { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+              { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+            ],
+            next: 'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20',
+            previous: null,
+          }),
+      })
+    );
+    render(<App />);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
+  });
+
 });
+
